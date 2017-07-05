db3ac9023d87842e434af74074eae93af0e9433b
Hamish Clulee <hamish.clulee@hotmail.com>
2017-07-05 20:43:06 +1200

    Successful bid made

--- src/components/content/AuctionBidding.vue
+++ src/components/content/AuctionBidding.vue
@@ -2,7 +2,9 @@
   <div>
     <!-- <dollar-spinner-small v-if="pending"></dollar-spinner-small> -->
     <div class="bidding-container"></div>
-    <a class="button" @click="tryConnect">Button</a>
+    <a class="button" @click="connectSocket">Connect</a>
+    <a class="button" @click="sendBid">Send Bid</a>
+  </div>
   </div>
 </template>
 
@@ -21,20 +23,33 @@ export default {
       msg: '',
       pending: true,
       ready: false,
-      invokeIdCnt: 0
+      invokeIdCnt: 0,
+      socketConnected: false
     }
   },
   methods: {
-    tryConnect() {
-      var sock = new SockJS('http://localhost:8080/bid');
-      console.log(sock)
+    connectSocket() {
+      
+    },
+    sendBid(){
+      this.stompClient.send("/app/bid", {}, JSON.stringify({'id': '1212121212'}));
+    }
+  },
+  mounted () {
+    var socket = new SockJS('http://localhost:8080/startBidding')
 
-      sock.onopen = function() {
-        console.log('open');
-        sock.send({id: "1212"});
-      };
+    this.stompClient = Stomp.over(socket)
 
-    } 
+    var that = this
+    this.stompClient.connect({}, function () {
+
+      this.socketConnected = true
+
+      that.stompClient.subscribe('/allBids/bidresponse', function (greeting) {
+        console.log(JSON.parse(greeting.body).content)
+      })
+
+    })
   }
 }
 
